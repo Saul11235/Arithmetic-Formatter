769175ab8fe88b4735d5554b7472ada006c48c42
Edwin Saul <edwinsaulpm@gmail.com>
2023-03-05 10:55:29 -0500

    first changes on funtion

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,13 +2,19 @@
 
 def problem_strings(string,calculate=False):
     content=list(filter( lambda x:x!="",str(string).split(" ")))
-    print(content)
-    return ["k","k"]
+    anchor=0
+    for x in content: anchor=max(anchor,len(x))
+    lista=[]
+    for x in content: lista.append(str(x))
+    lista.append("-"*(anchor+2))
+    if calculate:
+        lista.append("xxxx")
+    return lista
 
 
 
 def arithmetic_arranger(problems,calculate=False):
-    list_strings=[ problem_strings(x) for x in problems]
+    list_strings=[ problem_strings(x,calculate) for x in problems]
     arranged_problems=str(list_strings)
     return arranged_problems
 
@@ -17,4 +23,4 @@ def arithmetic_arranger(problems,calculate=False):
 if __name__=="__main__":
     # dev test
     print(problem_strings(" 1 + 3 "))
-    print(arithmetic_arranger([1,2,3]))
+    print(arithmetic_arranger(["1 + 2","23 - 11"],True))
