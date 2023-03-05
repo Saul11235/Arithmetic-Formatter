8ac1040f13240c8cb834153ac0e33a30aa3f5a8e
Edwin Saul <edwinsaulpm@gmail.com>
2023-03-04 22:49:36 -0500

    style changes

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,21 +2,19 @@
 
 def problem_strings(string,calculate=False):
     content=list(filter( lambda x:x!="",str(string).split(" ")))
-
     print(content)
     return ["k","k"]
 
 
 
-
-
 def arithmetic_arranger(problems,calculate=False):
     list_strings=[ problem_strings(x) for x in problems]
-
     arranged_problems=str(list_strings)
-
     return arranged_problems
 
+
+
 if __name__=="__main__":
+    # dev test
     print(problem_strings(" 1 + 3 "))
     print(arithmetic_arranger([1,2,3]))
