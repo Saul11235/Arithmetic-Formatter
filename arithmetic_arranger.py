db69fe34bd8ade9f4478da22f6c16cf54c157aa0
Edwin Saul <edwinsaulpm@gmail.com>
2023-03-04 22:45:42 -0500

    function code

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,6 +1,22 @@
-def arithmetic_arranger(problems):
-    # code by @Saul11235
-    arranged_problems=str(problems)
+# code by Edwin Saul @Saul11235
 
+def problem_strings(string,calculate=False):
+    content=list(filter( lambda x:x!="",str(string).split(" ")))
+
+    print(content)
+    return ["k","k"]
+
+
+
+
+
+def arithmetic_arranger(problems,calculate=False):
+    list_strings=[ problem_strings(x) for x in problems]
+
+    arranged_problems=str(list_strings)
 
     return arranged_problems
+
+if __name__=="__main__":
+    print(problem_strings(" 1 + 3 "))
+    print(arithmetic_arranger([1,2,3]))
